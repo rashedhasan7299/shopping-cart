699a3c08c80e650ac780082136205c24d5f5e47b
rashedhasan7299 <rashedhasan7299@gmail.com>
2020-05-01 14:14:21 +0600

    counter and price is changing but needs calculation adjustment

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
-// document.querySelector('.input-group').addEventListener("click", function(event) {
-//     var itemAmount = parseInt(document.querySelector(".form-control").value);
-//     if(event.target.classList.contains("fa-minus")) {
-//         itemAmount = itemAmount - 1;
-//         if(itemAmount>=0){
-//             document.querySelector(".form-control").value = itemAmount;
-//         }
-//     }
+let inputGroups = document.getElementsByClassName('input-group');
+let inputAreas = document.getElementsByClassName('form-control');
+let price = document.getElementsByClassName('price');
 
-//     if(event.target.classList.contains("fa-plus")) {
-//         itemAmount = itemAmount + 1;
-//         document.querySelector(".form-control").value = itemAmount;
-//     }
-// });
-
-var inputGroups = document.getElementsByClassName('input-group');
-var inputAreas = document.getElementsByClassName("form-control");
-console.log(inputAreas);
-
-for (let i = 0; i < inputGroups.length; i++) {
-    inputGroups[i].addEventListener("click", function(event) {
-        var itemAmount = parseInt(document.querySelector(".form-control").value);
+for (let i = 0; i <= inputGroups.length; i++) {
+    inputGroups[i].addEventListener('click', function(event) {
+        let currentItemAmount = parseInt(inputAreas[i].value);
         if(event.target.classList.contains("fa-minus")) {
-            itemAmount = itemAmount - 1;
-            if(itemAmount>=0){
-                document.querySelector(".form-control").value = itemAmount;
+            currentItemAmount = currentItemAmount - 1;
+            if(currentItemAmount>=0){
+                inputAreas[i].value = currentItemAmount;
             }
         }
 
         if(event.target.classList.contains("fa-plus")) {
-            itemAmount = itemAmount + 1;
-            document.querySelector(".form-control").value = itemAmount;
+            currentItemAmount = currentItemAmount + 1;
+            inputAreas[i].value = currentItemAmount;
         }
-    });
-    
+
+        let amount = amountPrice(i);
+        price[i].innerText = amount;
+    });   
+}
+
+function amountPrice(productNumber) {
+    productPrice = parseInt(price[productNumber].innerText);
+    totalProductPrice =  inputAreas[productNumber].value * productPrice;
+    return totalProductPrice;
 }
 
 
