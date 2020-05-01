eb614a5bea4afad2f7146733a3d5d839a9879a32
rashedhasan7299 <rashedhasan7299@gmail.com>
2020-05-01 20:28:09 +0600

    fixed individual total error

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let inputGroups = document.getElementsByClassName('input-group');
 let inputAreas = document.getElementsByClassName('form-control');
+
 let price = document.getElementsByClassName('price');
+let currentPrice = [];
+
+for (let i = 0; i < price.length; i++) {
+    priceValue = parseInt(price[i].innerText);
+    currentPrice.push(priceValue);    
+}
 
 for (let i = 0; i <= inputGroups.length; i++) {
+
+    //Increment Counter
     inputGroups[i].addEventListener('click', function(event) {
         let currentItemAmount = parseInt(inputAreas[i].value);
         if(event.target.classList.contains("fa-minus")) {
@@ -17,15 +26,17 @@ for (let i = 0; i <= inputGroups.length; i++) {
             inputAreas[i].value = currentItemAmount;
         }
 
-        let amount = amountPrice(i);
-        price[i].innerText = amount;
+        let productPrice = getIndividualPrice(i);
+        price[i].innerText = productPrice;
+
+        // subTotal(i);
     });   
 }
+//Get Individual Total for a product
 
-function amountPrice(productNumber) {
-    productPrice = parseInt(price[productNumber].innerText);
-    totalProductPrice =  inputAreas[productNumber].value * productPrice;
-    return totalProductPrice;
+function getIndividualPrice(i) {
+    currentProductPrice = currentPrice[i];
+    productNumber = inputAreas[i].value;
+    individualPrice = currentProductPrice * productNumber;
+    return individualPrice;
 }
-
-
